3529b7263d4133870316cb34f30c6df2ecca504e
Maksym Kryukov <mkryukov@magento.com>
2017-06-15 15:19:32 +0300

    Create adduser.sh

--- adduser.sh
+++ adduser.sh
@@ -8,15 +8,18 @@ groupadd chroot 2>/dev/null
 adduser --groups chroot,$2 $1 ;
 /bin/cp -fa /home/$1 /chroot/home/$1
 echo -e "$pass\n$pass" | (passwd --stdin $1)
-
-#
-echo "exit 0" > /home/$1/.bashrc
 chage -d 0 $1;
 
-echo "$1 member of"
+#DISABLE BASH NOT IN CHROOT
+echo "exit 0" > /home/$1/.bashrc
+
+#
+echo
+echo "$1 member of:"
 lid $1
 echo
 echo "$1 => $pass"
 
+#
 /bin/cp -fa /etc/passwd /chroot/etc/passwd 
 /bin/cp -fa /etc/group /chroot/etc/group 
